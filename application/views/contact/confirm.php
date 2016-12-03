a7fd8a055bfa2bf02b9167287b2b6ff6c63b5f6c
ryoikema <ryoikema@gmail.com>
2016-12-04 08:33:02 +0900

    Add files via upload

--- application/views/contact/confirm.php
+++ application/views/contact/confirm.php
@@ -1,23 +1,24 @@
   <div class="l-content">
     <div class="post">
-
       <h3>お問い合わせ 内容確認</h3>
-      <p>お問い合わせ内容はこちらでよろしいですか？</p>
-      <p>よろしければ「送信する」ボタンを押してください。</p>
+      <p class="confirm_text">
+        お問い合わせ内容はこちらでよろしいですか？<br>
+        よろしければ「送信する」ボタンを押してください。
+      </p>
 
       <?php echo form_open('contact/confirm') ?>
 
       <table class="contact_form">
         <tbody>
-          <tr>
+          <tr class="confirm_tr">
             <th><label for="name">お名前</label></th>
             <td><?php echo set_value('name'); ?></td>
           </tr>
-          <tr>
+          <tr class="confirm_tr">
             <th><label for="email">メールアドレス</label></th>
             <td><?php echo set_value('email'); ?></td>
           </tr>
-          <tr>
+          <tr class="confirm_tr">
             <th><label for="text">お問い合わせ内容</label></th>
             <td><?php echo set_value('text'); ?></td>
           </tr>
@@ -27,7 +28,7 @@
               <?php echo form_hidden('email', set_value('email')); ?>
               <?php echo form_hidden('text', set_value('text')); ?>
               <input type="submit" name="return_edit" value="戻る">
-              <input type="submit" name="submit" value="送信する">
+              <input type="submit" name="submit" value="送信">
             </td>
           </tr>
         </tbody>
